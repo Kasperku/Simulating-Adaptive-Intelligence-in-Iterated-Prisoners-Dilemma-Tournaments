f4d74fed03c8581dba4a67bd7df70aaeac4bc37a
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2025-01-03 02:31:13 -0800

    Minor refacotring BaseBot.py

--- model/bots/BaseBot.py
+++ model/bots/BaseBot.py
@@ -1,13 +1,12 @@
-"""
+from typing import Optional
+
+class BaseBot():
+    """
     Represents a generic bot for the Prisoner's Dilemma game.
 
     This is a base class for all bot strategies. It provides a structure for implementing
     specific strategies by defining common methods that can be overridden by subclasses.
-"""
-from typing import Optional
-from model.tournament.TurnObserver import TurnObserver
-
-class BaseBot(TurnObserver):
+    """
     def __init__(self, name):
         self.name = name
 
@@ -18,10 +17,6 @@ class BaseBot(TurnObserver):
     def choose_action(self, opponent_last_action: Optional[str]) -> str:
         """Defines the action the bot will take."""
         raise NotImplementedError("This method should be overridden by subclasses")
-
-    def notify_turn_result(self, my_action: str, opponent_action: str, opponent_name: str) -> None:
-        """Default implementation does nothing"""
-        pass
 
     # resets the internal state of the bot to its initial condition
     def reset(self) -> None:
